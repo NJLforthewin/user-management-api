a560b231d99664396abc7df7f878e17ea6ba1818
unknown <John Christian Sancija>
2025-02-20 15:19:57 +0800

    first commit

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,28 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
+import  { unserRoutes } from "./users/users.routes"
 
+// Load environment variables
 dotenv.config();
 
+// Validate PORT
 if (!process.env.PORT) {
     console.log(`No port value specified...`);
 }
 
 const PORT = parseInt(process.env.PORT as string, 10);
 
+// Initialize Express app
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
