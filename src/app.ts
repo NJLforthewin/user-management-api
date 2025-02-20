ba506d5b8a369e9ebb0689ef0395b9e5ad4135c3
unknown <YY239@ccs.local>
2025-02-20 18:21:53 +0800

    LAST COMMIT

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,28 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import helmet from "helmet";
-import  userRouter from "./users/users.routes"
+import express from "express"
+import * as dotevnv from "dotenv"
+import cors from "cors"
+import helmet from "helmet"
+import  userRouter  from "./users/users.routes";
+import { productRouter} from "./product/product.routes";
 
-// Load environment variables
-dotenv.config();
+dotevnv.config()
 
-// Validate PORT
 if (!process.env.PORT) {
-    console.log(`No port value specified...`);
+    console.log(`No port value specified...`)
 }
 
-const PORT = parseInt(process.env.PORT as string, 10);
+const PORT = parseInt(process.env.PORT as string, 10)
 
-// Initialize Express app
-const app = express();
+const app = express()
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(helmet());
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(cors())
+app.use(helmet())
 
 app.use('/', userRouter)
+app.use ('/', productRouter)
 
-// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+    console.log(`Server is listening on port ${PORT}`)
+})
